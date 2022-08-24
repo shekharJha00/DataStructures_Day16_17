ce2a8ee83ec3844a81b1d0885e682d6127a3475d
shekhar <shekharjha7417870578@gmail.com>
2022-08-24 21:28:10 +0530

    Added : Anagram detection program For Two String

--- src/com/bridgelabz/day16/AlgorithmUtility.java
+++ src/com/bridgelabz/day16/AlgorithmUtility.java
@@ -3,6 +3,7 @@ package com.bridgelabz.day16;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
 
 public class AlgorithmUtility {
     public static int stringBinarySearch(String[] arr, String key) {
@@ -76,7 +77,20 @@ public class AlgorithmUtility {
 
     }
 
-
+    public boolean isAnagram(String str1, String str2) {
+        char[] arr1 = str1.toCharArray();
+        char[] arr2 = str2.toCharArray();
+        if (str1.length() != str2.length()) {
+            return false;
+        } else {
+            Arrays.sort(arr1);
+            Arrays.sort(arr2);
+            if (Arrays.equals(arr1, arr2)) {
+                return true;
+            }
+        }
+        return false;
+    }
       }
 
 
---
+++ src/com/bridgelabz/day16/Anagram.java
@@ -0,0 +1,25 @@
+package com.bridgelabz.day16;
+
+import java.util.Scanner;
+
+public class Anagram {
+    public static void main(String[] args) {
+        String str1, str2;
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("enter string 1");
+        str1=scanner.next();
+        System.out.println("enter string 2");
+        str2=scanner.next();
+        AlgorithmUtility utility = new AlgorithmUtility();
+        boolean isAnagram=utility.isAnagram(str1,str2);
+        if(isAnagram) {
+            System.out.println("Two String are anagram");
+        }else {
+            System.out.println("Two String are not anagram");
+        }
+        scanner.close();
+    }
+
+}
+
+
